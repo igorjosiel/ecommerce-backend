871543e80450a0034e58ebd7940c7c5ae4014f5b
Igor Josiel <igorjosielvitalino@gmail.com>
2023-02-12 17:31:58 -0300

    feat: adicionando nova migration para a tabela dos administradores do sistema.

--- src/database/migrations/20230212202338_create_table_administrators.js
+++ src/database/migrations/20230212202338_create_table_administrators.js
@@ -3,16 +3,11 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-    return knex.schema.createTable('customers', function (table) {
+    return knex.schema.createTable('administrators', function (table) {
         table.increments('id');
         table.string('email', 255).notNullable();
         table.string('password', 255).notNullable();
         table.string('name', 255).notNullable();
-        table.string('cpf', 255).notNullable();
-        table.date('datebirth').notNullable();
-        table.string('gender', 1).notNullable();
-        table.string('country', 255).notNullable();
-        table.string('phonenumber', 20).notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
     });
@@ -22,4 +17,4 @@ exports.up = function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = knex => knex.schema.dropTable('customers');
+exports.down = () => knex.schema.dropTable('administrators');
